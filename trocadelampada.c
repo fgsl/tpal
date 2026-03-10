42e7836d5b4de58e68a742aab8b6e6204511e6d4
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2026-03-10 20:15:12 -0300

    Add lamp replacement procedure in trocadelampada.c

---
+++ trocadelampada.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+    int tem_alguem_para_segurar_a_escada = 0;
+
+
+    printf("Comprar a lâmpada nova\n");
+    printf("Desligar a energia\n");
+    printf("Pegar a escada\n");
+
+    if (tem_alguem_para_segurar_a_escada){
+        printf("Pedir para alguém segurar a escada"); 
+    } else {
+        printf("Colocar mesa para apoiar a escada"); 
+    }
+
+}
