d81aa605451adcbaabce6ddc5e57c6dcf62a09ef
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2026-03-25 20:25:00 -0300

    Add basic program to sum two floating-point numbers

---
+++ exercicio_47_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+	float a;
+	float b;
+	printf("Digite o valor de A:");
+	scanf("%f",&a);
+	printf("Digite o valor de B:");
+  scanf("%f",&b);
+    
+  printf("Resultado = %.2f\n",a+b);
+}
