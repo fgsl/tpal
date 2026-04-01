6f9b9c6f21ae0fba04c402ae212f9e7b4b9afde7
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2026-04-01 18:56:21 -0300

    Add vetordechar.c to print characters of an array

---
+++ vetordechar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+    char letras[] = {'h','e','l','l','o'};
+    
+    for (int posicao=0; posicao<5;posicao++)
+    {
+        printf("%c",letras[posicao]);
+    }
+}
