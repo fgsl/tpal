3e472ee50efcc86824e105a7de3dcc1d3b5f264f
Flávio Gomes da Silva Lisboa <flavio.lisboa@fgsl.eti.br>
2026-03-31 22:20:49 -0300

    Add files via upload

---
+++ jogodavelha.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main()
+{
+    char simbolo;
+    int jogador_venceu = 0, todas_as_casas_preenchidas = 0;
+    char tabuleiro[3][3] = {
+        {' ',' ',' '},
+        {' ',' ',' '},
+        {' ',' ',' '},
+    };
+    int linha,coluna;
+
+    do {
+        printf("Qual símbolo você deseja usar? ");
+        scanf("%c",&simbolo);
+    }
+    while (simbolo != 'O' && simbolo != 'X');
+    printf("\nVocê escolheu o símbolo %c",simbolo);
+    do {
+        printf("\nQual é a linha? ");
+        scanf("%d",&linha);
+        printf("\nQual é a coluna? ");
+        scanf("%d",&coluna);
+        tabuleiro[linha][coluna] = simbolo;
+        for(int i=0;i<3;i++)
+        {
+            for(int j=0;j<3;j++)
+            {
+                printf("%c",tabuleiro[i][j]);
+            }
+            printf("\n");
+        }        
+    } while (!jogador_venceu && 
+    !todas_as_casas_preenchidas);
+}
